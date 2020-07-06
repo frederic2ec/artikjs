4dd543e5a0e60ead7efcfb2f9e2fe839dfd8cc3c
Ticass <thrifthshopdowntheroad@gmail.com>
2020-07-06 08:29:01 -0400

    Finished arrays

--- src/arrays.js
+++ src/arrays.js
@@ -35,8 +35,99 @@ export function findex(array, callback){
 };
 
 export function feach (array, callback) {
-     array.forEach(element => callback(element))        //for Each in array
+     return array.forEach(element => callback(element))        //for Each in array
 };
+
+export function from (array, ...args) { 
+    return array.from(args);
+};
+
+export function has (array, ...args){
+    return array.includes(args)
+};
+
+export function iof(array, ...args){
+    return array.indexOf(args)
+};
+
+export function isArr(array, ...args){
+    return array.isArray(args)
+};
+
+export function join(array, separator){
+    return array.join(separator)
+};
+
+export function keys(array){
+    return array.keys();
+};
+
+export function length(array){
+    return array.length();
+};
+
+export function lindexOf(array, ...args){
+    return array.lastindexOf(args);
+};
+
+export function map(array, callback){
+    return array.map(element => callback(element))
+};
+
+export function Rlast(array){
+    return array.pop()
+};
+
+export function push(array, ...args){
+    return array.push(args)
+};
+
+export function reduce(array, callback){
+    return array.reduce(element => callback(element));
+};
+
+export function reduceR(array, callback){
+    return array.reduceRight(element => callback(element));
+};
+
+export function reverso(array){
+    return array.reverse()
+};
+
+export function Rfirst(array){
+    return array.shift();
+};
+
+export function slice(array, start, end){
+    return array.slice(start, end)
+};
+
+export function some(array, callback){
+    return array.some(element => callback(element));
+};
+
+export function sort(array, callback){
+    return array.sort(element => callback(element));
+};
+
+export function splice(array, ...args){
+    return array.splice(args);
+};
+
+export function toString(array){
+    return array.toString();
+}
+
+export function unshift(array, ...args){
+    return array.unshift(args);
+};
+
+export function ValOf(array){
+    return array.valueOf();
+};
+
+
+
 
 
 
