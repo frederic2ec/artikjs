647fd9c7240c5781feb6e4eea7a6c4fc4ecb23e5
Ticass <thrifthshopdowntheroad@gmail.com>
2020-07-06 09:03:03 -0400

    Finished arrays, recompiled the library

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@ import { getJSON } from "./ajax";
 
 import { alert, cprint, write, wprint } from "./output";
 
-import { igloo, cct, cin, iter, all, fill, filter, aFind, findex, feach} from "./arrays";
+import { igloo, cct, cin, iter, all, fill, filter, aFind, findex, feach, from, has,  iof, isArr, join, keys, length, lindexOf, map, Rlast, reduce, reduceR, reverso, Rfirst, slice, some, sort, splice, toString, unshift, ValOf} from "./arrays";
 
-export { getJSON, alert, cprint, write, wprint, igloo, cct, cin, iter, all, fill, filter, aFind, findex, feach}
+export { getJSON, alert, cprint, write, wprint, igloo, cct, cin, iter, all, fill, filter, aFind, findex, feach, from, has,  iof, isArr, join, keys, length, lindexOf, map, Rlast, reduce, reduceR, reverso, Rfirst, slice, some, sort, splice, toString, unshift, ValOf}
 
 
