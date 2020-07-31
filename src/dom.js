b33c5ec95696c46c08e51b79c0909a088490fcdb
Ticass <thrifthshopdowntheroad@gmail.com>
2020-07-31 17:59:00 -0400

    :sparkles: added some dom properties to be tested and started the demo website

--- src/dom.js
+++ src/dom.js
@@ -1,13 +1,23 @@
 function dom(selector) {
   const self = {
     element: document.querySelector(selector),
-    html: () => self.element.innerHTML,
+    html: () => self.element,
     on: (event, callback) => {
       self.element.addEventListener(event, callback);
     },
     hide: () => {
       self.element.style.display = "none";
     },
+    keyup: () => {
+      self.element.onkeyup;
+    },
+    //attributes
+    val: () => {
+      self.element.value;
+    },
+    name: () =>{
+      self.element.name;
+    }
   };
   return self;
 }
