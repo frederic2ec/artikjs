0561e75355df9e0000d1a6490f4749599b81e14e
Alexis <thrifthshopdowntheroad@gmail.com>
2020-09-01 10:04:01 -0400

    :sparkles: Added Some DOM functions

--- src/dom.js
+++ src/dom.js
@@ -11,7 +11,15 @@ function dom(selector) {
     keyup: () =>  self.element.onkeyup,
     val: () => self.element.value,
     setAttribute: (attr, String) => self.element.setAttribute(attr, String),
+    getNamedItem: (nodename) => self.element.getNamedItem(nodename),
+    aKey: () => self.element.accessKey,
+    attr: () => self.element.attributes,
+    blur: () => self.element.blur(),
+    ChildElCount: () => self.element.childElementCount,
+    
 
+
+    
   };
   return self;
 }
