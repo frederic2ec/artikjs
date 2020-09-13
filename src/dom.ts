382cc139263b94dc1528326374ff2541f51b9324
Alexis <thrifthshopdowntheroad@gmail.com>
2020-09-12 23:10:33 -0400

    :sparkles: almost completed the DOM element

--- src/dom.ts
+++ src/dom.ts
@@ -1,13 +1,17 @@
 function dom(selector: any) {
   const self = {
     element: document.querySelector(selector),
+    documentElement: document.documentElement,
     html: () => self.element.innerHTML,
-    on: (event: string, callback: any) => {
-      self.element.addEventListener(event, callback);
+    on: (...args: any[]) => {
+      let callback: any = args[args.length - 1]
+      args = args[args.length - 2]
+      self.element.addEventListener(args, callback)
     },
-    hide: () => {
-      self.element.style.display = "none";
+    off: (event: string, callback: any) => {
+      self.element.removeEventListener(event, callback);
     },
+    hide: () => self.element.style.display = "none",
     keyup: () =>  self.element.onkeyup,
     val: () => self.element.value,
     setAttribute: (attr: string, String: string) => self.element.setAttribute(attr, String),
@@ -31,12 +35,51 @@ function dom(selector: any) {
     firstChild: () => self.element.firstChild(),
     firstElementChild: () => self.element.firstElementChild,
     focus: () => self.element.focus(),
-    getAttribute: (attr: any) => self.element.getAttribute(attr),
-
-    
-
-
-    
+    getAttribute: (attr: string) => self.element.getAttribute(attr),
+    getAttributeNode: (attr: string) => self.element.getAttributeNode(attr),
+    hasAttribute: (attr: string) => self.element.hasAttribute(attr),
+    hasChildNodes: () => self.element.hasChildNodes(),
+    hasAttributes: () => self.element.hasAttributes(),
+    id: () => self.element.id,
+    text: () => self.element.innerText,
+    insertAdjacentElement: (pos: string, element: string) => self.element.insertAdjacentElement(pos, element),
+    insertAdjancentHTML: (pos: string, text: string) => self.element.insertAdjancentHTML(pos, text),
+    insertAdjancentText: (pos: string, text: string) => self.element.insertAdjancentText(pos, text),
+    isContentEditable: () => self.element.isContentEditable,
+    isSameNode: (item: string) => self.element.isSameNode(item),
+    lang: () => self.element.lang,
+    lastChild: () => self.element.lastChild,
+    lastElementChild: () => self.element.lastElementChild,
+    matches: (...args: any) => self.element.matches(args),
+    namespaceURI: () => self.element.namespaceURI,
+    nextSibling: () => self.element.nextSibling,
+    nextElementSibling: () => self.element.nextElementSibling,
+    nodeName: () => self.element.nodeName,
+    nodeType: () => self.element.nodeType,
+    nodeValue: () => self.element.nodeValue,
+    normalize: () => self.element.normalize,
+    offsetHeight: () => self.element.offsetHeight,
+    offsetWidth: () => self.element.offsetWidth,
+    offsetLeft: () => self.element.offsetLeft,
+    offsetParent: () => self.element.offsetParent,
+    offsetTop: () => self.element.offsetTop,
+    oHtml: () => self.element.outerHTML,
+    outerText: () => self.element.outerText,
+    ownerDocument: () => self.element.ownerDocument,
+    parentNode: () => self.element.parentNode,
+    parentElement: () => self.element.parentElement,
+    previousSibling: () => self.element.previousSibling,
+    previousElementSibling: () => self.element.previousElementSibling,
+    removeAttribute: (attr: string) => self.element.removeAttribute(attr),
+    removeAttributeChildNode: (attributenode: string) => self.element.removeAttributeChildNode(attributenode),
+    removeChild: (node: string) => self.element.removeChild(node),
+    replaceChild: (node: string, nnode: string) => self.element.replaceChild(node, nnode),
+    requestFullScreen: () => self.element.requestFullScreen(),
+    scrollHeight: () => self.element.scrollHeight,
+    scrollIntoView: () => self.element.scrollIntoView,
+    scrollLeft: () => self.element.scrollLeft,
+    scrollTop: () => self.element.scrollTop,
+    scrollWidth: (alignto: string) => self.element.scrollIntoView(alignto),
   };
   return self;
 }
